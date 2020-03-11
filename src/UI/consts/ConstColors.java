156c9018d48f7e3759ba4fac3f5b969f7c28154a
antoine-white <antoine.maxime.bl@gmail.com>
2020-03-11 09:40:51 +0100

    change constantes

--- src/UI/consts/ConstColors.java
+++ src/UI/consts/ConstColors.java
@@ -2,7 +2,7 @@ package UI.consts;
 
 import java.awt.Color;
 
-public static class ConstColors{
+public class ConstColors{
     public static final Color BACKGROUND = Color.WHITE;
     public static final Color TEXT = Color.BLACK;
 }
---
+++ src/UI/consts/ConstFonts.java
@@ -0,0 +1,8 @@
+package UI.consts;
+
+import java.awt.Font;
+
+public class ConstFonts{
+    public static final Font DEFAULT = new Font(" TimesRoman ",Font.PLAIN,10);
+    public static final Font TITLE = new Font(" TimesRoman ",Font.BOLD,30);
+}
