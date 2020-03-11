585e689032ee9c4e87d49bfd84b41d49e38939b9
antoine-white <antoine.maxime.bl@gmail.com>
2020-03-11 10:08:13 +0100

    add border

--- src/UI/consts/ConstColors.java
+++ src/UI/consts/ConstColors.java
@@ -5,4 +5,5 @@ import java.awt.Color;
 public class ConstColors{
     public static final Color BACKGROUND = Color.WHITE;
     public static final Color TEXT = Color.BLACK;
+    public static final Color BORDER = Color.BLACK;
 }
