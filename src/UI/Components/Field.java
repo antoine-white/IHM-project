959061910a38006511667ba173979273feceef5e
antoine-white <antoine.maxime.bl@gmail.com>
2020-03-11 10:08:26 +0100

    add field file

---
+++ src/UI/Components/Field.java
@@ -0,0 +1,20 @@
+package UI.Components;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+import UI.consts.*;
+
+public class Field extends JPanel{
+
+    private static final Dimension MIN_SIZE = new Dimension(200,200);
+    private static final Dimension INITIAL_SIZE = new Dimension(400,400);
+    private static final int[] DEFAULT_CELL = {100,100};
+
+    public Field(){
+        super();
+        this.setPreferredSize(Field.INITIAL_SIZE);
+        this.setMinimumSize(Field.MIN_SIZE);
+        this.setBorder(BorderFactory.createLineBorder(ConstColors.BORDER));
+    }
+}
